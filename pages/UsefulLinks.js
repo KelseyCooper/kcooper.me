6dd8bf7315c7e7630014d2c9a94b39b5dca1362d
KelseyCooper <kelsey@kcooper.me>
2022-01-27 20:02:29 -0800

    Add design inspo link

--- pages/UsefulLinks.js
+++ pages/UsefulLinks.js
@@ -1,5 +1,6 @@
 const UsefulLinks = () => <ul>
   <section className="section">
+    <li><a href="https://monopo.vn/">Design insporation</a></li>
     <li><a href="https://webdeasy.de/en/top-css-buttons-en/">Button style</a></li>
     <li><a href="https://www.joshwcomeau.com/css/make-beautiful-gradients/">Gradient generator</a></li>
   </section>
