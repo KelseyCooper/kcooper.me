967eb8050182a1554017363f664e93d9b9c9a58c
KelseyCooper <kelsey@kcooper.me>
2022-01-28 19:51:58 -0800

    Add vercel BlyrHash patch

--- next.config.js
+++ next.config.js
@@ -4,3 +4,15 @@ module.exports = {
     domains: ["via.placeholder.com", "www.fillmurray.com"],
   },
 };
+
+// Vercel NextBlurHash patch
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
